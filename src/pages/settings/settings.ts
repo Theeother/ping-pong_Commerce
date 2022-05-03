8c29877aca360835db5e3be1b3f3c407ecc5caa0
Theeother <44754872+Theeother@users.noreply.github.com>
2022-05-03 17:58:10 +0100

    sala7t setting

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -21,13 +21,13 @@ export class SettingsPage {
 
 
   constructor(public navCtrl: NavController, private sqlite:SQLite) {
-    this.createdatabaseFile();
+    this.createDatabaseFile();
   }
   private createDatabaseFile(): void {
     this.sqlite.create({
     name: DATABASE_FILE_NAME,
     location: 'default'
-    })
+    }) 
     .then((db: SQLiteObject) => {
     console.log('Bdd créée !');
     this.db = db;
@@ -36,10 +36,45 @@ export class SettingsPage {
     .catch(e => console.log(e));
     }
   private createTables(): void {
-      this.db.executeSql('CREATE TABLE IF NOT EXISTS `produits` ( `idProduit` INTEGER NOT NULL, `name` TEXT NOT NULL,  `desc` TEXT, `categoryId` INTEGER, PRIMARY KEY(`idMovies`), FOREIGN KEY(`categoryId`) REFERENCES idCategories )', {})
+      this.db.executeSql('CREATE TABLE IF NOT EXISTS `produits` ( \
+        `idProduit` INTEGER NOT NULL,\
+        `name` TEXT NOT NULL,\
+        `desc` TEXT,\
+        `prix` TEXT,\
+        `imagePath` TEXT,\
+        `isOn` INTEGER DEFAULT 1,\
+        PRIMARY KEY(`idProduit`))',{})
       .then(() => {
-      console.log('Table Movies created !');
-    
+      console.log('Table Produits created !');
+      })
+      .catch(e => console.log(e));
+    }
 
+    public saveMyProd() {
+    
+      // INSERT INTO `CATEGORIES` (name) VALUES('Test');
+      // INSERT INTO `MOVIES`(name, eval, desc, categoryId) VALUES ('Nom film', 3, 'Description', 1)
+      this.db.executeSql('INSERT INTO `produits`(name, desc, prix , imagePath,isOn) VALUES (\'' + this.name + '\', '+
+      this.description +', \''+ this.prix +', \''+ this.imgPath +', \''+ this.isOn.valueOf()   +'\', last_insert_rowid())', {})
+      .then(() => console.log('Movie inserted !'))
+      .catch(e => console.log(e));
+      }
+      
+    public retrieveFilms() {
+        this.products = [];
+        this.db.executeSql('SELECT name FROM `produits`', {})
+        .then((data) => {
+        if(data == null) {
+        return;
+        }
+        if(data.rows) {
+        if(data.rows.length > 0) {
+        for(var i = 0; i < data.rows.length; i++) {
+        this.products.push(data.rows.item(i).name);
+        }
+        }
+        }
+        });
+        }
 
 }
